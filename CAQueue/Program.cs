40ff6e75dbf6f0d0ce39c3cb2e9ff2534dc56952
Mahmoud-Mokhtar <mahmoudmoukhtar93@gmail.com>
2026-05-05 18:57:21 +0300

    initial

---
+++ CAQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Queue<PrintingJop> printingJops = new Queue<PrintingJop>();
+        printingJops.Enqueue(new PrintingJop("documentation.docx", 2));  //Enqueue() enter to queue
+        printingJops.Enqueue(new PrintingJop("user-stories.pdf", 6));
+        printingJops.Enqueue(new PrintingJop("report.xlsx", 6));
+        printingJops.Enqueue(new PrintingJop("payroll.report", 5));
+        printingJops.Enqueue(new PrintingJop("budget.xlsx", 4));
+        printingJops.Enqueue(new PrintingJop("algorithm.ppt", 1));
+
+        Console.WriteLine($"Current  Before while Queue Count: {printingJops.Count}");
+
+        Random rnd = new Random();
+        while (printingJops.Count > 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            var job = printingJops.Dequeue(); // Dequeue() delete F-O queue
+            Console.WriteLine($"printing ... [{job}]");
+            System.Threading.Thread.Sleep(rnd.Next(1, 5) * 1000);
+        }
+        Console.WriteLine($"Current after while Queue Count: {printingJops.Count}");
+    }
+
+class PrintingJop
+{
+    private readonly string _file;
+    private readonly int _copies;
+
+    public PrintingJop(string file, int copies)
+    {
+        _file = file;
+        _copies = copies;
+    }
+
+    public override string ToString()
+    {
+        return $"{_file} x #{_copies} copies";
+    }
+}
---
+++ CAStacks/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System.Diagnostics.Contracts;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("STACKS");
+
+        Stack<Command> undo = new Stack<Command>();
+        Stack<Command> redo = new Stack<Command>();
+
+        string line;
+
+        while (true)
+        {
+            Console.Write("URL (EXIT TO 'QUIT') : ");
+            line = Console.ReadLine().ToLower();
+
+            if (line == "exit")
+                break;
+            else if (line == "back")
+                if (undo.Count > 0)
+                {
+                    var item = undo.Pop();
+                    redo.Push(item);
+                }
+                else
+                {
+                    continue;
+                }
+            else if (line == "forward")
+                if (redo.Count > 0)
+                {
+                    var item = redo.Pop();
+                    undo.Push(item);
+                }
+                else
+                {
+                    continue;
+                }
+            else
+            {
+                // add url line
+                undo.Push(new Command (line));
+            }
+            Console.Clear();
+            Print("back", undo);
+            Print("forward", redo);
+
+        }//end of while
+
+
+        Stack<int> numbers = new Stack<int>(new List<int> { 1, 2, 3 });
+
+        while (numbers.Count > 0)
+        {
+            var n = numbers.Peek();  // peek() only pointer to the top of a stack without delete otherwise pop() is deleting the top of a stack
+
+            Console.WriteLine(n);
+        }
+
+
+
+    } // END OF MAIN
+    public static void Print(string name,Stack<Command> commands)
+    {   
+        Console.WriteLine($"{name} History");
+        Console.BackgroundColor = name.ToLower() == "back" ? ConsoleColor.DarkGreen : ConsoleColor.Magenta;
+        foreach (var u in commands)
+        {
+            Console.WriteLine($"\t{u}");
+        }
+        Console.BackgroundColor = ConsoleColor.Black;
+
+
+
+    } // end of method
+} // END OF PROGRAM
+
+class Command
+{
+    private readonly DateTime _createat;
+    private readonly string _url;
+
+    public Command(string _url)
+    {
+        _createat = DateTime.Now;
+        this._url = _url;
+    }
+    public override string ToString()
+    {
+        return $"[{this._createat.ToString("yyyy-MM-dd hh:mm")}] {this._url}";
+    }
+}
